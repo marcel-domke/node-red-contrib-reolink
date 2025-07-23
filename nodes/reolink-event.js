c4a8675b10c7a58ed5977da6ceae244815a6978d
Marcel Domke <169958024+marcel-domke@users.noreply.github.com>
2025-07-23 17:53:40 +0200

    Added event node

---
+++ nodes/reolink-event.js
@@ -0,0 +1,58 @@
+/*
+Copyright 2025 Marcel Domke
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+module.exports = function (RED) {
+    function ReolinkEventNode(config) {
+        RED.nodes.createNode(this, config);
+
+        const node = this;
+        const server = RED.nodes.getNode(config.server);
+        node.queryTimeout = null;
+        node.lastEventStates = null;
+
+        // Fetch data and send to output
+        async function queryStates() {
+            try {
+                const eventStates = await server.queryCommand("GetEvents");
+                if (eventStates && JSON.stringify(eventStates) !== JSON.stringify(node.lastEventStates)) {
+                    node.send({
+                        payload: eventStates,
+                        topic: config.topic || server.name
+                    });
+                    node.lastEventStates = eventStates;
+                }
+                node.status(server.connectionStatus);
+            } catch (error) {
+                console.warn("Error during query: ", error.message);
+                node.status({ fill: "red", shape: "ring", text: `Error: Query failed` });
+            }
+
+            // Set timeout to call queryStates again
+            node.queryTimeout = setTimeout(queryStates, 2000);
+        }
+
+        // Start first query
+        queryStates();
+
+        node.on("close", () => {
+            if (node.queryTimeout) {
+                clearTimeout(node.queryTimeout);
+            }
+        });
+    }
+
+    RED.nodes.registerType("reolink-event", ReolinkEventNode);
+};
